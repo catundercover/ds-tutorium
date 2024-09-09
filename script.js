2f8893b27b2956306738f5a09e5567ca8b004bd9
catundercover <148898269+catundercover@users.noreply.github.com>
2024-09-09 22:44:41 +0200

    added some of the files

---
+++ script.js
@@ -0,0 +1,19 @@
+let darkmode = localStorage.getItem('dark-mode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const  enableDarkmode = () => {
+    document.body.classList.add('dark-mode')
+    localStorage.setItem('dark-mode', 'active')
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('dark-mode')
+    localStorage.setItem('dark-mode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('dark-mode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
